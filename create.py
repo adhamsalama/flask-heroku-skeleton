cfbc42215095eceeafd3b91cd7294c653f21886e
adham <adhamsalama666@gmail.com>
2020-03-25 11:51:15 +0200

    Created create.py to create a db and 2 tables

---
+++ create.py
@@ -0,0 +1,14 @@
+# Creates a db and 2 tables for users and feedback
+
+import sqlite3
+
+con = sqlite3.connect('tables.db')
+db = con.cursor()
+
+
+with open('tables.sql') as f:
+    tables = f.readlines()
+    for table in tables:
+        db.execute(table)
+    con.commit()
+    con.close()
